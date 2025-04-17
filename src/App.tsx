0efbad6205c0e94b44a4388c1160e489baea8af8
jungkukhwa <95341917+jungkukhwa@users.noreply.github.com>
2025-04-17 11:04:42 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
 import KakaoMap from "./component/kakkaoMap";
 import "./App.css";
-import { useEffect, useState } from "react";
+
 function App() {
-  // const [lat, setLat] = useState<string | null>(null);
-  // const [lng, setLng] = useState<string | null>(null);
-
-  // useEffect(() => {
-  //   const params = new URLSearchParams(window.location.search);
-  //   setLat(params.get("lat"));
-  //   setLng(params.get("lng"));
-  // }, []);
-
   return <KakaoMap />;
 }
 
