3ed40044120f06a049c0413f0125c1784f1002f8
jungkukhwa <95341917+jungkukhwa@users.noreply.github.com>
2025-04-17 11:13:47 +0900

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/kakaomapTest/", // <= 레포 이름과 일치해야 해!
   plugins: [react()],
-})
+});
